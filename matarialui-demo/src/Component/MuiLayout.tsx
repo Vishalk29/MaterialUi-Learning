c6141e7b04f06f7ffcf15f6e74e7bbbe8572a244
Vishal k <vkulkarni149@gmail.com>
2022-07-20 13:58:29 +0530

    Learned about the Stack layout component

--- matarialui-demo/src/Component/MuiLayout.tsx
+++ matarialui-demo/src/Component/MuiLayout.tsx
@@ -1,19 +1,33 @@
-import { Box } from "@mui/material"
+import { Box, Stack, Divider } from "@mui/material"
 
 const MuiLayout = () => {
     return (
-        <Box sx={{
-            backgroundColor: 'primary.main',
-            color: 'white',
-            height: '100px',
-            width: '100px',
-            padding: '16px',
-            '&:hover': {
-                backgroundColor: 'secondary.light'
-            },
-        }}>
-            Hello world
-        </Box>
+        <Stack sx={{ border: '1px solid ' }} direction='row' spacing={2} divider={<Divider orientation="vertical" flexItem />}>
+            <Box sx={{
+                backgroundColor: 'primary.main',
+                color: 'white',
+                height: '100px',
+                width: '100px',
+                padding: '16px',
+                '&:hover': {
+                    backgroundColor: 'secondary.light'
+                },
+            }}>
+                Hello world
+            </Box>
+            <Box sx={{
+                backgroundColor: 'secondary.main',
+                color: 'white',
+                height: '100px',
+                width: '100px',
+                padding: '16px',
+                '&:hover': {
+                    backgroundColor: 'secondary.light'
+                },
+            }}>
+                Hello world
+            </Box>
+        </Stack>
     )
 }
 
